140979003d364f8129b1c627e47c93c73f52cc5a
Greg Bowler <greg@g105b.com>
2026-03-07 14:25:55 +0000

    refactor: reduce class complexity, improve code quality (#233)
    * build: require phpcs/phpmd
    
    * maintenance: refactor to reduce complexity of main class
    closes #16
    
    * build: php8.1 compatability
    
    * ci: phpcs and phpmd
    
    * ci: codacy
    
    * ci: codacy ignore girhub

---
+++ src/AttributeSelectorConverter.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Gt\CssXPath;
+
+class AttributeSelectorConverter {
+	/** @param array<string, mixed> $token */
+	public function apply(
+		array $token,
+		XPathExpression $expression,
+		bool $htmlMode
+	):void {
+		$expression->ensureElement();
+
+		$attribute = (string)$token["content"];
+		if($htmlMode) {
+			$attribute = strtolower($attribute);
+		}
+
+		$detail = $token["detail"] ?? null;
+		$detailType = $detail[0] ?? null;
+		$detailValue = $detail[1] ?? null;
+
+		if(!$this->hasEqualsType($detailType)) {
+			$expression->appendFragment("[@{$attribute}]");
+			return;
+		}
+
+		$valueString = trim((string)$detailValue["content"], " '\"");
+		$equalsType = $detailType["content"];
+		$expression->appendFragment(
+			$this->buildExpression($attribute, $valueString, $equalsType)
+		);
+	}
+
+	/** @param array<string, mixed>|null $detailType */
+	private function hasEqualsType(?array $detailType):bool {
+		return isset($detailType["type"])
+			&& $detailType["type"] === "attribute_equals";
+	}
+
+	private function buildExpression(
+		string $attribute,
+		string $value,
+		string $equalsType
+	):string {
+		return match($equalsType) {
+			Translator::EQUALS_EXACT => "[@{$attribute}=\"{$value}\"]",
+			Translator::EQUALS_CONTAINS => "[contains(@{$attribute},\"{$value}\")]",
+			Translator::EQUALS_CONTAINS_WORD => "["
+				. "contains(concat(\" \",@{$attribute},\" \"),"
+				. "concat(\" \",\"{$value}\",\" \"))"
+				. "]",
+			Translator::EQUALS_OR_STARTS_WITH_HYPHENATED => "["
+				. "@{$attribute}=\"{$value}\" or "
+				. "starts-with(@{$attribute}, \"{$value}-\")"
+				. "]",
+			Translator::EQUALS_STARTS_WITH => "["
+				. "starts-with(@{$attribute}, \"{$value}\")"
+				. "]",
+			Translator::EQUALS_ENDS_WITH => "["
+				. "substring(@{$attribute},"
+				. "string-length(@{$attribute}) - "
+				. "string-length(\"{$value}\") + 1)"
+				. "=\"{$value}\""
+				. "]",
+			default => "[@{$attribute}]",
+		};
+	}
+}
---
+++ test/phpunit/ComponentConverterTest.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Gt\CssXPath\Test;
+
+use Gt\CssXPath\AttributeSelectorConverter;
+use Gt\CssXPath\PseudoSelectorConverter;
+use Gt\CssXPath\SingleSelectorConverter;
+use Gt\CssXPath\ThreadMatcher;
+use Gt\CssXPath\Translator;
+use Gt\CssXPath\XPathExpression;
+use PHPUnit\Framework\TestCase;
+
+class ComponentConverterTest extends TestCase {
+	public function testThreadMatcherCollatesAttributeDetailParts():void {
+		$matcher = new ThreadMatcher();
+		$thread = array_values(
+			$matcher->collate(Translator::CSS_REGEX, "[name='email']")
+		);
+
+		self::assertSame("attribute", $thread[0]["type"]);
+		self::assertSame("name", $thread[0]["content"]);
+		self::assertSame("attribute_equals", $thread[0]["detail"][0]["type"]);
+		self::assertSame("=", $thread[0]["detail"][0]["content"]);
+		self::assertSame("attribute_value", $thread[0]["detail"][1]["type"]);
+		self::assertSame("'email'", $thread[0]["detail"][1]["content"]);
+	}
+
+	public function testThreadMatcherUsesTransformForPrimaryAndDetailMatches():void {
+		$matcher = new ThreadMatcher();
+		$thread = array_values(
+			$matcher->collate(
+				Translator::CSS_REGEX,
+				"[name=email]",
+				fn(string $type, string $content):array => [
+					"type" => strtoupper($type),
+					"content" => "v:" . $content,
+				]
+			)
+		);
+
+		self::assertSame("ATTRIBUTE", $thread[0]["type"]);
+		self::assertSame("v:name", $thread[0]["content"]);
+		self::assertSame("ATTRIBUTE_EQUALS", $thread[0]["detail"][0]["type"]);
+		self::assertSame("v:=", $thread[0]["detail"][0]["content"]);
+	}
+
+	public function testPseudoSelectorConverterContainsRequiresSpecifier():void {
+		$converter = new PseudoSelectorConverter();
+		$expression = new XPathExpression(".//");
+		$expression->appendElement("p", true);
+
+		$converter->apply(
+			["type" => "pseudo", "content" => "contains"],
+			null,
+			$expression
+		);
+		self::assertSame(".//p", $expression->toString());
+
+		$converter->apply(
+			["type" => "pseudo", "content" => "contains"],
+			["type" => "pseudospecifier", "content" => "'Example'"],
+			$expression
+		);
+		self::assertSame(".//p[contains(text(),'Example')]", $expression->toString());
+	}
+
+	public function testPseudoSelectorConverterNthChildCanRefineExistingPredicate():void {
+		$converter = new PseudoSelectorConverter();
+		$expression = new XPathExpression(".//");
+		$expression->appendElement("li", true);
+		$expression->appendFragment("[contains(@class,\"selected\")]");
+
+		$converter->apply(
+			["type" => "pseudo", "content" => "nth-child"],
+			["type" => "pseudospecifier", "content" => "2"],
+			$expression
+		);
+
+		self::assertSame(
+			".//li[contains(@class,\"selected\") and position() = 2]",
+			$expression->toString()
+		);
+	}
+
+	public function testAttributeSelectorConverterHonoursHtmlModeCaseSensitivity():void {
+		$converter = new AttributeSelectorConverter();
+
+		$htmlExpression = new XPathExpression(".//");
+		$converter->apply(
+			["type" => "attribute", "content" => "DATA-FOO"],
+			$htmlExpression,
+			true
+		);
+		self::assertSame(".//*[@data-foo]", $htmlExpression->toString());
+
+		$xmlExpression = new XPathExpression("//");
+		$converter->apply(
+			["type" => "attribute", "content" => "DATA-FOO"],
+			$xmlExpression,
+			false
+		);
+		self::assertSame("//*[@DATA-FOO]", $xmlExpression->toString());
+	}
+
+	public function testAttributeSelectorConverterBuildsHyphenatedPrefixExpression():void {
+		$converter = new AttributeSelectorConverter();
+		$expression = new XPathExpression(".//");
+
+		$converter->apply(
+			[
+				"type" => "attribute",
+				"content" => "lang",
+				"detail" => [
+					["type" => "attribute_equals", "content" => "|="],
+					["type" => "attribute_value", "content" => "en"],
+				],
+			],
+			$expression,
+			true
+		);
+
+		self::assertSame(
+			".//*[@lang=\"en\" or starts-with(@lang, \"en-\")]",
+			$expression->toString()
+		);
+	}
+
+	public function testSingleSelectorConverterHandlesWildcardClassAndIdSelectors():void {
+		$converter = new SingleSelectorConverter();
+
+		self::assertSame(
+			".//*[@id='the-title']",
+			$converter->convert("#the-title", ".//", true)
+		);
+		self::assertSame(
+			".//*[contains(concat(' ',normalize-space(@class),' '),' c-menu ')]",
+			$converter->convert(".c-menu", ".//", true)
+		);
+	}
+
+	public function testSingleSelectorConverterSupportsPseudoTextOnInput():void {
+		$converter = new SingleSelectorConverter();
+
+		self::assertSame(
+			".//input[@type=\"text\"]",
+			$converter->convert("input:text", ".//", true)
+		);
+	}
+
+	public function testXPathExpressionMarksAxisTransitionsBeforeEnsuringElement():void {
+		$expression = new XPathExpression(".//");
+		$expression->appendElement("main", true);
+		$expression->appendFragment("/");
+		$expression->markElementMissing();
+		$expression->ensureElement();
+
+		self::assertSame(".//main/*", $expression->toString());
+	}
+}
